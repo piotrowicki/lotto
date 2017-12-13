e0dda6ada1060705e4679dac9add3a344bd4fb48
piotrowicki <piotrowicki@gmail.com>
2017-12-13 19:55:42 +0100

    Initialize findAll query in DrawDao

--- src/main/java/pl/piotrowicki/lotto/dao/DrawDao.java
+++ src/main/java/pl/piotrowicki/lotto/dao/DrawDao.java
@@ -1,9 +1,10 @@
 package pl.piotrowicki.lotto.dao;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import pl.piotrowicki.lotto.entity.Draw;
 
 /**
@@ -13,7 +14,10 @@ import pl.piotrowicki.lotto.entity.Draw;
 @Stateless
 public class DrawDao implements Serializable {
     
+    @PersistenceContext
+    private EntityManager em;
+    
     public List<Draw> findAll() {
-        return new ArrayList<>();
+        return em.createNamedQuery("Draw.findAll").getResultList();
     }
 }
