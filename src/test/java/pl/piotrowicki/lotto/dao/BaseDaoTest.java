61f36d93ce15d90dccafc54d5223843f80143d25
piotrowicki <piotrowicki@gmail.com>
2018-02-28 16:58:02 +0100

    Refactor h2 unit tests

--- src/test/java/pl/piotrowicki/lotto/dao/BaseDaoTest.java
+++ src/test/java/pl/piotrowicki/lotto/dao/BaseDaoTest.java
@@ -1,19 +1,35 @@
 package pl.piotrowicki.lotto.dao;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 
 /**
  * @author piotrowicki <piotrowicki at gmail.com>
  */
 public class BaseDaoTest {
 
+    private static EntityManagerFactory emf;
     protected EntityManager em;
 
+    @BeforeClass
+    public static void createEntityManagerFactory() {
+        emf = Persistence.createEntityManagerFactory("lotto-test");
+    }
+    
     @Before
     public void init() {
-        em = Persistence.createEntityManagerFactory("lotto-test").createEntityManager();
+        em = emf.createEntityManager();
         em.getTransaction().begin();
     }
+    
+    @After
+    public void rollback() {
+        em.getTransaction().rollback();
+        em.close();
+    }
+    
 }
