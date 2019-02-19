a14d12f3a9ba49e473df7a0ca68767999be9f2e3
piotrowicki <piotrowicki@gmail.com>
2019-02-19 13:09:19 +0100

    Fix DB schema to match realm needs

--- src/main/java/pl/piotrowicki/lotto/entity/UserEntity.java
+++ src/main/java/pl/piotrowicki/lotto/entity/UserEntity.java
@@ -1,46 +1,43 @@
 package pl.piotrowicki.lotto.entity;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import javax.persistence.CascadeType;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import org.hibernate.annotations.NaturalId;
 
 /**
  *
  * @author piotrowicki <piotrowicki at gmail.com>
  */
 @Entity
-@Table(name = "USER")
+@Table(name = "USERS")
 public class UserEntity extends BaseEntity {
 
     private static final long serialVersionUID = -5170288537131044056L;
-   
-    @Column(name = "USERNAME")
+
+    @NaturalId
+    @Column(name = "USERNAME", nullable = false)
     private String username;
-    
+
     @Column(name = "PASSWORD")
     private String password;
-    
+
     @Column(name = "EMAIL")
     private String email;
-    
+
     @Embedded
     private Address address;
-    
+
     @Column(name = "LAST_LOGON")
     private LocalDateTime lastLogon;
-    
-    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    private List<RoleEntity> roles;
 
     public String getUsername() {
         return username;
     }
-    
+
     public void setUsername(String username) {
         this.username = username;
     }
@@ -77,11 +74,20 @@ public class UserEntity extends BaseEntity {
         this.lastLogon = lastLogon;
     }
 
-    public List<RoleEntity> getRoles() {
-        return roles;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof UserEntity)) {
+            return false;
+        }
+        UserEntity userEntity = (UserEntity) o;
+        return this.username.equals(userEntity.username);
     }
 
-    public void setRoles(List<RoleEntity> roles) {
-        this.roles = roles;
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.username);
     }
 }
---
+++ src/main/java/pl/piotrowicki/lotto/entity/UserRolesEntity.java
@@ -0,0 +1,58 @@
+package pl.piotrowicki.lotto.entity;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import org.hibernate.annotations.NaturalId;
+
+/**
+ *
+ * @author piotrowicki <piotrowicki at gmail.com>
+ */
+@Entity
+@Table(name = "USER_ROLES")
+public class UserRolesEntity extends BaseEntity {
+
+    private static final long serialVersionUID = -4783666319759511245L;
+
+    @NaturalId
+    @Column(name = "USERNAME", nullable = false)
+    private String username;
+
+    @Column(name = "ROLE_NAME")
+    private String roleName;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof UserRolesEntity)) {
+            return false;
+        }
+        UserRolesEntity userRolesEntity = (UserRolesEntity) o;
+        return this.username.equals(userRolesEntity.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.username);
+    }
+}
