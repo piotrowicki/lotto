425f5a769195804e22dbd158300b6c859162f802
Piotr Nowicki <piotrowicki@gmail.com>
2020-04-13 13:44:10 +0200

    Update jee version 8

---
+++ src/main/java/pl/piotrowicki/lotto/config/AppConfig.java
@@ -0,0 +1,10 @@
+package pl.piotrowicki.lotto.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@FacesConfig
+@ApplicationScoped
+public class AppConfig {
+    
+}
