ace0c58a4805d8b97a1e183873da97615c497ede
Piotr Nowicki <piotrowicki@gmail.com>
2020-11-08 11:17:37 +0100

    Revert "Fix API read"
    This reverts commit 10da9b8515f20bab5c237b517bb1feccbb992687.

--- src/main/java/pl/piotrowicki/lotto/job/BaseDrawJob.java
+++ src/main/java/pl/piotrowicki/lotto/job/BaseDrawJob.java
@@ -1,15 +1,14 @@
 package pl.piotrowicki.lotto.job;
 
 import java.time.LocalDate;
-
 import pl.piotrowicki.lotto.entity.draw.BaseDrawEntity;
 
 /**
+ *
  * @author nowik
  */
 public class BaseDrawJob<T extends BaseDrawEntity> {
-    public <T extends BaseDrawEntity> T convertToEntity(Class<T> clazz, String input) throws InstantiationException, IllegalAccessException {
-        input = input.replaceAll("(\\r|\\n)", " ");
+     public <T extends BaseDrawEntity> T convertToEntity(Class<T> clazz, String input) throws InstantiationException, IllegalAccessException {
         String[] splittedDraw = input.split(" ");
 
         int firstSpace = input.indexOf(" ") + 1;
@@ -18,5 +17,5 @@ public class BaseDrawJob<T extends BaseDrawEntity> {
         entity.setDrawDate(LocalDate.parse(splittedDraw[0]));
         entity.setNumbers(input.substring(firstSpace));
         return entity;
-    }
+    }   
 }
